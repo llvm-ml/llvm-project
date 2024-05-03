2790ff669058480929ea23cbbbc5aa5a55e91ab7
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2024-05-02 22:23:19 -0700

    Add -DNDEBUG and O3 to mass input gen

--- llvm/tools/input-gen/input-gen.cpp
+++ llvm/tools/input-gen/input-gen.cpp
@@ -333,6 +333,7 @@ public:
         Args.push_back("-O0");
       } else {
         Args.push_back("-O3");
+        Args.push_back("-DNDEBUG");
       }
       std::string ErrMsg;
       int Res = sys::ExecuteAndWait(
