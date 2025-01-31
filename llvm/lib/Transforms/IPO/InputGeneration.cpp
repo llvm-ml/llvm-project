7a0049b67270b50cdb709ca0bd1df0c548d650f1
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-01-31 18:46:26 +0900

    Populate some stubs

--- llvm/lib/Transforms/IPO/InputGeneration.cpp
+++ llvm/lib/Transforms/IPO/InputGeneration.cpp
@@ -14,6 +14,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/IPO/InputGeneration.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Transforms/IPO/InputGenerationImpl.h"
 
@@ -21,22 +22,55 @@ using namespace llvm;
 
 #define DEBUG_TYPE "inputgen"
 
-static cl::opt<IGInstrumentationModeTy>
-    ClInstrumentationMode("input-gen-mode", cl::desc("Instrumentation mode"),
-                          cl::Hidden, cl::init(IG_Disabled),
-                          cl::values(clEnumValN(IG_Disabled, "disable", ""),
-                                     clEnumValN(IG_Record, "record", ""),
-                                     clEnumValN(IG_Generate, "generate", ""),
-                                     clEnumValN(IG_Replay, "replay", "")));
+static cl::opt<IGIMode> ClInstrumentationMode(
+    "input-gen-mode", cl::desc("Instrumentation mode"), cl::Hidden,
+    cl::init(IGIMode::Disabled),
+    cl::values(clEnumValN(IGIMode::Disabled, "disable", ""),
+               clEnumValN(IGIMode::Record, "record", ""),
+               clEnumValN(IGIMode::Generate, "generate", ""),
+               clEnumValN(IGIMode::Replay, "replay", "")));
 
 namespace llvm {
+
+using EI = InputGenEntryInstrumenter;
+using MI = InputGenMemoryInstrumenter;
+
+void EI::instrumentFunction(Module &M, Function &F) {
+  // TODO
+}
+
+void EI::instrumentAll(Module &M) {
+  SmallVector<Function *> ToInstrument;
+  for (Function &F : M)
+    ToInstrument.push_back(&F);
+  for (Function *F : ToInstrument)
+    instrumentFunction(M, *F);
+}
+
+bool EI::instrumentMarkedEntries(Module &M) {
+  SmallVector<Function *> ToInstrument;
+  for (Function &F : M)
+    if (F.hasFnAttribute(llvm::Attribute::InputGenEntry))
+      ToInstrument.push_back(&F);
+  if (ToInstrument.size() == 0)
+    return false;
+  for (Function *F : ToInstrument)
+    instrumentFunction(M, *F);
+  return true;
+}
+
+bool MI::instrument(Module &M) { return true; }
 
 InputGenInstrumentEntriesPass::InputGenInstrumentEntriesPass() = default;
 
 PreservedAnalyses
 InputGenInstrumentEntriesPass::run(Module &M, AnalysisManager<Module> &MAM) {
-  if (ClInstrumentationMode == IG_Disabled)
+  if (ClInstrumentationMode == IGIMode::Disabled)
     return PreservedAnalyses::all();
+  InputGenEntryInstrumenter IGEI(ClInstrumentationMode);
+  if (IGEI.instrumentMarkedEntries(M))
+    // FIXME PreservedAnalyses
+    return PreservedAnalyses::none();
   return PreservedAnalyses::all();
 }
 
@@ -44,8 +78,12 @@ InputGenInstrumentMemoryPass::InputGenInstrumentMemoryPass() = default;
 
 PreservedAnalyses
 InputGenInstrumentMemoryPass::run(Module &M, AnalysisManager<Module> &MAM) {
-  if (ClInstrumentationMode == IG_Disabled)
+  if (ClInstrumentationMode == IGIMode::Disabled)
     return PreservedAnalyses::all();
+  InputGenMemoryInstrumenter IGMI(ClInstrumentationMode);
+  if (IGMI.instrument(M))
+    // FIXME PreservedAnalyses
+    return PreservedAnalyses::none();
   return PreservedAnalyses::all();
 }
 } // namespace llvm
