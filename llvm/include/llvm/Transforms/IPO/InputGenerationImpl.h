3d54aa3e576f7ea6ce897388d40b041aff9a56d6
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-01-31 18:46:26 +0900

    Populate some stubs

--- llvm/include/llvm/Transforms/IPO/InputGenerationImpl.h
+++ llvm/include/llvm/Transforms/IPO/InputGenerationImpl.h
@@ -9,15 +9,50 @@
 // This file defines the implementation for the input-gen instrumentation that
 // can be used by tools as well.
 //
+// The EntryInstrumenter must be ran before the MemoryInstrumenter.
+//
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_TRANSFORMS_INSTRUMENTATION_INPUTGENERATIONIMPL_H
 #define LLVM_TRANSFORMS_INSTRUMENTATION_INPUTGENERATIONIMPL_H
 
+#include "llvm/IR/Function.h"
 
 namespace llvm {
 
-enum IGInstrumentationModeTy { IG_Record, IG_Generate, IG_Replay, IG_Disabled };
+enum class IGIMode : unsigned { Record, Generate, Replay, Disabled };
+
+/// Prepares the entry points into functions for generation, recording, or
+/// replaying.
+class InputGenEntryInstrumenter {
+public:
+  InputGenEntryInstrumenter(IGIMode Mode) : Mode(Mode) {}
+
+  /// Instruments only a specific function.
+  void instrumentFunction(Module &M, Function &F);
+
+  /// Instruments functinos marked by the inputgen_entry attribute.
+  bool instrumentMarkedEntries(Module &M);
+
+  /// Instruments all functions.
+  void instrumentAll(Module &M);
+
+private:
+  IGIMode Mode;
+};
+
+/// Instruments the memory effects of all functions with prepared by
+/// InputGenEntryInstrumenter entries.
+class InputGenMemoryInstrumenter {
+public:
+  InputGenMemoryInstrumenter(IGIMode Mode) : Mode(Mode) {}
+
+  /// Instruments
+  bool instrument(Module &M);
+
+private:
+  IGIMode Mode;
+};
 
 } // namespace llvm
 
