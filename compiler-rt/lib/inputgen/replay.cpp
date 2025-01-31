8469a9a9901afefc63a6f19b9bc2fbb44ef2d7c9
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-01-31 13:58:49 +0900

    Add stubs for input-gen rts

---
+++ compiler-rt/lib/inputgen/replay.cpp
@@ -0,0 +1,13 @@
+//===--- replay.cpp - Emit LLVM Code from ASTs for a Module ---------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements replaying input-gen's recorded or generated inputs.
+//
+//===----------------------------------------------------------------------===//
+
+int main(int argc, char **argv) { return 0; }
