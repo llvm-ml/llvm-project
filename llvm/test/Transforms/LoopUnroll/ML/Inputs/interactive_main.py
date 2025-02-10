928e9b9516b665af1a7c70cb3082f496603fafba
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-02-10 13:31:25 +0900

    WIP unroll instrumentation

--- llvm/test/Transforms/LoopUnroll/ML/Inputs/interactive_main.py
+++ llvm/test/Transforms/LoopUnroll/ML/Inputs/interactive_main.py
@@ -5,6 +5,7 @@ import sys
 def main(args):
     class Advisor:
         to_return = False
+        counter = 0
 
         def advice(self, _):
             l = [0.5 for _ in range(1 + 32 - 2)]
@@ -24,15 +25,23 @@ def main(args):
             action = bool(int.from_bytes(fc.read(1)))
             print(action)
             fc.readline()
-            # Respond that we do not want instrumentation
-            tc.write(bytes([0]))
-            tc.flush()
-
+            if args[2] == 'instrument':
+                f.write(bytes([1]))
+                begin = ("test_loop_begin_" + str(self.counter)).encode('ascii') + bytes([0])
+                end = ("test_loop_end_" + str(self.counter)).encode('ascii') + bytes([0])
+                self.counter += 1
+                f.write(begin)
+                f.write(end)
+                f.flush()
+            else:
+                # Respond that we do not want instrumentation
+                tc.write(bytes([0]))
+                tc.flush()
 
     a = Advisor()
-    interactive_host.run_interactive(args[2],
+    interactive_host.run_interactive(args[3],
                                      a.advice,
-                                     args[3:],
+                                     args[4:],
                                      a.before_advice,
                                      a.after_advice)
 
--- llvm/test/Transforms/LoopUnroll/ML/development-mode.ll
+++ llvm/test/Transforms/LoopUnroll/ML/development-mode.ll
@@ -4,14 +4,14 @@
 ; RUN: cp %S/../../../../lib/Analysis/models/log_reader.py %t.rundir
 ; RUN: cp %S/../../../../lib/Analysis/models/interactive_host.py %t.rundir
 ; RUN: cp %S/Inputs/interactive_main.py %t.rundir
-; RUN: %python %t.rundir/interactive_main.py 3 2.0 %t.nchannel-basename \
+; RUN: %python %t.rundir/interactive_main.py 3 2.0 no %t.nchannel-basename \
 ; RUN:    opt %S/Inputs/nested.ll -S -O3 --mlgo-loop-unroll-interactive-channel-base=%t.nchannel-basename \
 ; RUN:    --mlgo-loop-unroll-advisor-mode=development \
 ; RUN:    --interactive-model-runner-echo-reply \
 ; RUN:    -debug-only=loop-unroll-development-advisor \
 ; RUN:    -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK1
 
-; RUN: %python %t.rundir/interactive_main.py 3 0.5 %t.nchannel-basename \
+; RUN: %python %t.rundir/interactive_main.py 3 0.5 no %t.nchannel-basename \
 ; RUN:    opt %S/Inputs/nested.ll -S -O3 --mlgo-loop-unroll-interactive-channel-base=%t.nchannel-basename \
 ; RUN:    --mlgo-loop-unroll-advisor-mode=development \
 ; RUN:    --interactive-model-runner-echo-reply \
