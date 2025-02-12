b4b4600fc867e7d514c66bb43896f0e16b519206
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-02-12 22:06:55 +0900

    Fix compilation flags

--- input-gen/compile.sh
+++ input-gen/compile.sh
@@ -6,8 +6,8 @@ basen=${1%.*}
 reco=$basen.rec.o
 replo=$basen.repl.o
 
-clang -O3 -flto -c $1 -o $reco  -mllvm -enable-input-gen
-clang -O3 -flto -c $1 -o $replo
+clang -O3 -flto -c $1 -o $reco -mllvm --input-gen-mode=generate
+clang -O3 -flto -c $1 -o $replo -mllvm --input-gen-mode=replay
 clang++ ig.cpp vm_obj.cpp vm_storage.cpp recorder.cpp -g -std=c++20 -O3 $reco -o $basen.rec -flto -fuse-ld=lld -fno-exceptions -DNDEBUG
 clang++ ig.cpp vm_obj.cpp vm_storage.cpp recorder.cpp -g -std=c++20 -O3 $reco -o $basen.rec.dbg -flto -fuse-ld=lld -fno-exceptions
 clang++ vm_storage.cpp replay.cpp  -g -std=c++20 -O3 $replo -o $basen.repl -flto -fuse-ld=lld -fno-exceptions -DNDEBUG
